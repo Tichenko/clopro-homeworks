d212248ce1c7b1f9c25a9e95e14865b965dfb794
Tatyana Ilchenko <12967707-TIlchenko@users.noreply.gitlab.com>
2024-03-31 20:01:09 +0300

    Организация сети

--- terraform/output.tf
+++
@@ -1,6 +0,0 @@
-output "output" {
-  value = {
-    "public_ip" = "${yandex_compute_instance.public-vm.network_interface[0].nat_ip_address}"
-    "private_ip"  = "${yandex_compute_instance.private-vm.network_interface[0].ip_address}"
-  }
-}
